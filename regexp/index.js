b142ff1bdce034b28b0a2c051b5aabee5f9d435b
RomanTaran <73136963+RomanTaran@users.noreply.github.com>
2020-11-04 10:31:28 +0200

    Update index.js

--- regexp/index.js
+++ regexp/index.js
@@ -25,8 +25,8 @@ document.querySelectorAll('[data-show]').forEach(function (button) {
 		document.querySelector('#description').classList.add('d-none');
 		document.querySelector('#preview').classList.add('d-none');
 		var descriptionString = document.querySelector('#description').value;
-		var rule1 = /\+\+([a-zA-Zа-яА-ЯёЁ]+)\+\+/;
-		var rule2 = /\-\-([a-zA-Zа-яА-ЯёЁ]+)\-\-/;
+		var rule1 = /\+\+(\S+)\+\+/;
+		var rule2 = /\-\-(\S+)\-\-/;
 		var rule3 = /\((https\:\/\/[a-zA-Z]+\-[a-zA-Z]+\.[a-zA-Z]+\/[a-zA-Z]+\.(?:jpg|png))\)/;
 		var rule4 = /(https\:\/\/[a-zA-Z]+\-[a-zA-Z]+\.[a-zA-Z]+\/[a-zA-Z]+\/[a-zA-Z]+)/;
 		document.querySelector('#preview').innerHTML = descriptionString.replace(rule1, '<strong>$1<strong>').replace(rule2, '<i>$1<i>').
