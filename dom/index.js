bea03d3260b7ec2877338e65f36d2256dd601a1b
RomanTaran <73136963+RomanTaran@users.noreply.github.com>
2020-11-10 16:56:22 +0200

    Update index.js

--- dom/index.js
+++ dom/index.js
@@ -3,12 +3,48 @@ jQuery('input').on('paste', function (e) {
 
 	var text = e.originalEvent.clipboardData.getData('text/plain');
 	var input = e.currentTarget;
-
+	var headersLetters = ['C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+	var arr = text.split('\n');
+	var inputString = [];
+	for (let i = 0; i < arr.length; i++) {
+		inputString[i]=arr[i].split(';');
+	}
+	var numberColumns;
+	var numberRows;
+	if(input.name === 'a1' || input.name === 'b1') numberRows = arr.length-2;
+	else numberRows = arr.length-1;
+	if (input.name === 'a1' || input.name === 'a2') numberColumns = inputString[0].length-2;
+	else numberColumns = inputString[0].length-1;
+	for (let i = 0; i < numberRows; i++) {
+		$('tbody').append('<tr><td>'+Number(i+3)+'</td><td><input type=\"text\" name=\"a'+Number(i+3)+'\"' +
+		' value=\"0\"/></td><td><input type=\"text\" name=\"b'+Number(i+3)+'\" value=\"0\"/></td></tr>');
+	}
+	for (let i = 0; i < numberColumns; i++) {
+		$('thead>tr').append('<th>'+headersLetters[i]+'</th>');
+		$('tbody>tr').append(function(j) {
+			return '<td><input  type=\"text\" name=\"'+headersLetters[i].toLowerCase()+Number(j+1)+'\" value=\"0\"/></td>';
+		});
+	}
+	var inputs = $('input');
+	var indexStart;
+	for (let i = 0; i < $('input').length; i++) {
+		if (inputs[i].name == input.name) indexStart = i;
+	}
+	var k;
+	for (let i = 0; i < inputString.length; i++) {
+		for (let j = 0; j < inputString[i].length; j++) {
+			inputs[indexStart++].value=inputString[i][j];
+		}
+	}
 
 });
 
-jQuery('th').on('contextmenu', function (e) {
+var currentColumn;
+
+jQuery('thead th').on('contextmenu', function (e) {
 	e.preventDefault();
+
+	currentColumn = e.currentTarget;
 
 	var menu = jQuery('#column-menu');
 
