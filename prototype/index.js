8e0ab1ecda741cc3db74a326e498c95bbd81ef65
RomanTaran <73136963+RomanTaran@users.noreply.github.com>
2020-11-16 17:16:59 +0200

    Update index.js

--- prototype/index.js
+++ prototype/index.js
@@ -58,3 +58,64 @@ function Csv() {
         return str;
     };
 }
+
+//Наследуем CsvArray от Array
+CsvArray.prototype = Object.create(Array.prototype);
+//Создаем конструктор CsvArray
+function CsvArray() {
+    this.parse = function (string, separator) {
+        let arr = string.trim().split('\n');
+
+        //Объявляем функцию поиска разделителя
+        function getSeparator(array, sym) {
+            let separator;
+            sym.forEach(function (element) {
+                let arrSymbol = [];
+                array.forEach(function (elem) {
+                    let count = 0;
+                    let position = elem.indexOf(element);
+                    while (position !== -1) {
+                        count++;
+                        position = elem.indexOf(element, position + 1)
+                    }
+                    arrSymbol.push(count);
+                });
+                if (arrSymbol.every(function (el) {
+                    return el == arrSymbol[0];
+                }) && arrSymbol[0] != 0) {
+                    separator = element;
+                }
+            })
+            return separator;
+        }
+
+        //Определяем, какой у нас будет разделитель
+        if (separator == undefined) {
+            let symbols = [',', ';', '\t'];
+            separator = getSeparator(arr, symbols);
+        }
+        //Разбиваем каждый элемент массива на подмассивы по найденному разделителю
+        let inputString = [];
+        for (let i = 0; i < arr.length; i++) {
+            inputString[i] = arr[i].split(separator);
+        }
+        //Определяем количество строк и столбцов в будущей таблице
+        let numberRows = arr.length;
+        let numberColumns = inputString[0].length;
+        //Создаем таблицу
+        for (let i = 0; i < numberRows; i++) {
+            let row = [];
+            for (let j = 0; j < numberColumns; j++) {
+                row.push(inputString[i][j]);
+            }
+            this.push(row);
+        }
+    }
+    this.generate = function (separator){
+        let str = this[0].join(separator);
+        for(let i = 1; i < this.length; i++){
+            str += "\n" + this[i].join(separator);
+        }
+        return str;
+    };
+}
