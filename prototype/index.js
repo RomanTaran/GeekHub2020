5c3fe4d825bcc2e475a8b679c1ad5763a3984e24
RomanTaran <73136963+RomanTaran@users.noreply.github.com>
2020-11-21 15:04:46 +0200

    Update index.js

--- prototype/index.js
+++ prototype/index.js
@@ -17,7 +17,7 @@ Csv.prototype.parse = function parse(string, separator) {
         }
         arrSymbol.push(count);
       });
-      if (arrSymbol.every((el) => {return el === arrSymbol[0];}) && arrSymbol[0] !== 0) {
+      if (arrSymbol.every((el => el === arrSymbol[0]) && arrSymbol[0] !== 0) {
         separator = element;
       }
     });
@@ -74,7 +74,7 @@ CsvArray.prototype.parse = function parse(string, separator) {
         }
         arrSymbol.push(count);
       });
-      if (arrSymbol.every((el) => {return el === arrSymbol[0];}) && arrSymbol[0] !== 0) {
+      if (arrSymbol.every(el => el === arrSymbol[0]) && arrSymbol[0] !== 0) {
         separator = element;
       }
     });
