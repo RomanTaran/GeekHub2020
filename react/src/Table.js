36dcedf4cfb713ce4a149aad391dce63b8071224
roman <romantaran.taran@gmail.com>
2020-12-01 00:38:09 +0200

    First redaction

---
+++ react/src/Table.js
@@ -0,0 +1,56 @@
+import React from 'react';
+
+export default function Table(props) {
+  let {columns, rows, cell, data} = props;
+  const alpha = 'abcdefghijklmnopqrstuvwxyz';
+
+  function addHeader(cols) {
+    const headers = [];
+    for (let i = 0; i < cols; i++) {
+      headers.push(<td>{alpha[i].toUpperCase()}</td>);
+    }
+    return headers;
+  }
+
+  function addRows(rowNum) {
+    const row = [];
+    for (let i = 0; i < rows; i++) {
+      row.push(<tr>
+        <td>{i + 1}</td>
+        {addCells(i, props.columns)}</tr>);
+    }
+    return row;
+  }
+
+  function addCells(rowNum, cols) {
+    const cells = [];
+    for (let i = 0; i < cols; i++) {
+      cells.push(<td><input type='text' name={alpha[i] + (rowNum + 1)} defaultValue={insertValue(rowNum, i)}/></td>)
+    }
+    return cells;
+  }
+
+  function insertValue(rowIndex, colIndex) {
+    if (props.data === undefined || props.cell === undefined) {
+      return;
+    } else if (rowIndex < props.cell[1] - 1 || colIndex < alpha.indexOf(props.cell[0])) {
+      return;
+    } else {
+      return props.data[rowIndex - props.cell[1] + 1][colIndex - alpha.indexOf(props.cell[0])];
+    }
+  }
+
+  return (
+    <table>
+      <thead>
+      <tr>
+        <td>&nbsp;</td>
+        {addHeader(props.columns)}
+      </tr>
+      </thead>
+      <tbody>
+      {addRows(props.rows)}
+      </tbody>
+    </table>
+  );
+};
