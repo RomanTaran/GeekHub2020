e2e85a1f5d8acfb2046a8338b0f02b7b846ea7df
RomanTaran <73136963+RomanTaran@users.noreply.github.com>
2021-02-15 23:42:23 +0200

    Update index.js

--- react-node-express/server/index.js
+++ react-node-express/server/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const app = express();
+const {resolve} = require("path")
 
 mongoose.connect("mongodb://localhost/todos-db",{useUnifiedTopology:true, useNewUrlParser:true});
 
